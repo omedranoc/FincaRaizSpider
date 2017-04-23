f9e341d0f370e9d77ab31feaf512a13cd6da35d9
Saul Medrano <saulmedrano@carfax.com>
2017-04-23 17:43:22 -0500

    precio estrato ... printed

--- SpiderFincaRaiz.py
+++ SpiderFincaRaiz.py
@@ -1,14 +1,14 @@
 import scrapy
 from scrapy.selector import Selector
 from pandas import Series, DataFrame
-
+import re
 
 import scrapy
 
 
 class SpiderFincaRaiz(scrapy.Spider):
     name = "SpiderFincaRaiz"
-    #scrapy shell "https://www.fincaraiz.com.co/apartamentos/venta/bogota/?ad=30|1||||1||8|||67|3630001|||300000000|||||||||||||1|||1||||||-1"
+    
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0',
     }
@@ -22,27 +22,27 @@ class SpiderFincaRaiz(scrapy.Spider):
     def parse(self, response):
         
         page = Selector(response).css('div .span-title a::attr(href)').extract()
-        print "saul"
+        
 
         for href in page:
             yield scrapy.Request(response.urljoin(href),callback=self.parse_apartment)
         
     def parse_apartment(self, response):
-        
+        #run > scrapy runSpider SpiderFincaRaiz
+        line = Selector(response).css('dt.property.bold.dt::text')[1].extract()
+        line2 = Selector(response).css('dt.property.bold.dt::text')[2].extract()
+        print "*****************************************" 
+        print response.url
+        print "Precio Total: " ,Selector(response).css('span.display.bold::text')[0].extract()
+        # display bold 
         if Selector(response).css('dt.property.bold.dt::text')[3].extract() == 'Estrato:':
-            print 'sas'
-
-       
-        # table = [[1 , 2], [3, 4]]
-        # onclick=announcements.css('al[class="advert  Product_Code_DON AD_OV"]::attr(onclick)').extract()
-
-        
-        # print "place" ,len(onclick)
-        # frame = DataFrame({'title':onclick},columns=['onclick'])
-        
-       
-        
-        
-        # with open(r'out14.csv', 'a') as f:
-        #            frame.to_csv(f, encoding='utf-8',header=False)
-      
+            print "Estrato: "+Selector(response).css('dd.dd::text')[3].extract()
+        if  Selector(response).css('dt.property.bold.dt::text')[0].extract() == u'\xC1rea Const.:':
+            print "Area construida "+ Selector(response).css('dd.dd::text')[0].extract()
+        if  Selector(response).css('dt.property.bold.dt::text')[1].extract() == u'\xC1rea privada:':
+            print"Area privada "+ Selector(response).css('dd.dd::text')[1].extract()
+        if  'Precio metro cuadrado' in line :
+            print "Precio : " , Selector(response).css('dd.dd::text')[1].extract()  
+        if  'Precio' in line2:
+            print "Precio metro cuadrado: " + Selector(response).css('dd.dd::text')[2].extract()   
+        print "*****************************************"  
